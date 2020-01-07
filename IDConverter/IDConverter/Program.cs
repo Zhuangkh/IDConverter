d993a1028c0066007da62b6325e1712fe7a17024
Einsam <einsam0413@gmail.com>
2020-01-08 01:05:54 +0800

    初始化项目并提交项目代码

---
+++ IDConverter/IDConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System;
+
+namespace IDConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("GUID is null!");
+                Console.ReadKey();
+            }
+            else
+            {
+                string id = args[0];
+                int count = id.Length;
+                switch (count)
+                {
+                    case 22:
+                        Console.WriteLine("IFC GUID: " + id);
+                        Console.WriteLine("Back to .NET GUID: " + IfcGuidConverter.FromIfcGuid(id).ToString());
+                        break;
+                    case 36:
+                        Console.WriteLine(".NET GUID: " + id.ToString());
+                        Console.WriteLine("IFC GUID: " + IfcGuidConverter.ToIfcGuid(new Guid(id)));
+                        break;
+                    case 45:
+                        Console.WriteLine("Revit UniqueId: " + id);
+                        Guid guid = RevitIDConverter.ToGuid(id);
+                        Console.WriteLine(".NET GUID: " + guid.ToString());
+                        Console.WriteLine("IFC GUID: " + IfcGuidConverter.ToIfcGuid(guid));
+                        break;
+                    default:
+                        Console.WriteLine("GUID Length Error!");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ IDConverter/IDConverter/RevitIDConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IDConverter
+{
+    public class RevitIDConverter
+    {
+        public static Guid ToGuid(string uniqueId)
+        {
+            if (uniqueId.Length != 45)
+            {
+                throw new Exception("the given string isn't revit unique id");
+            }
+            int elementId = int.Parse(uniqueId.Substring(37), NumberStyles.AllowHexSpecifier);
+            int tempId = int.Parse(uniqueId.Substring(28, 8), NumberStyles.AllowHexSpecifier);
+            int xor = tempId ^ elementId;
+            return new Guid(uniqueId.Substring(0, 28) + xor.ToString("x8"));
+        }
+    }
+}
